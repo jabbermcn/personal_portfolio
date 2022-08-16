a64fac66854fbb9099c435204fe40826dd2800f4
mikhailouski_n <jabber_mcn@tut.by>
2022-08-16 15:26:06 +0300

    add detail.html, Resume.pdf

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Blog
 
 
@@ -6,3 +6,8 @@ def all_blogs(request):
     """Собираем все объекты модели и отображаем на странице"""
     blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
