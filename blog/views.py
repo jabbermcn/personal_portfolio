0ebe2b27537a108c0c2170115985612f79fde19b
mikhailouski_n <jabber_mcn@tut.by>
2022-08-17 16:09:37 +0300

    add base.html, some changes at html files

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from blog.models import Blog
 
 def all_blogs(request):
     """Собираем все объекты модели и отображаем на странице"""
-    blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
